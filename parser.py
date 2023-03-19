727c7834445af1959e0564ec5c9106a86157c15f
Quimzy <bam.boy2020@gmail.com>
2023-03-19 22:01:06 +0300

    better reader (using control transfer)

--- parser.py
+++ parser.py
@@ -6,6 +6,8 @@ import os
 
 DEBUG = os.getenv('DEBUG')
 
+DEV_TO_HOST = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_CLASS, usb.util.CTRL_RECIPIENT_INTERFACE)
+
 class Reader:
     def __init__(self, idv=0x054c, idp=0x09cc):
         self.dev = usb.core.find(idVendor=idv, idProduct=idp)
@@ -22,23 +24,14 @@ class Reader:
             self.intf = 0
             self.detach()
 
-        self.dev.set_configuration()
-        self.ep = self.get_ep()
-    
     def detach(self, id=0):
         if self.dev.is_kernel_driver_active(id):
             self.dev.detach_kernel_driver(id)
             print(f"Interface number {id} detached from kernel")
     
-    def get_ep(self): 
-        ep = self.dev[0][(self.intf,0)][0]
+    def read(self, report_id, size):
+        data = self.dev.ctrl_transfer(DEV_TO_HOST, 0x01, report_id, 0, size+1)[1:].tobytes()
 
-        return ep
-    
-    def read(self):
-        eaddr = self.ep.bEndpointAddress
-        data = self.dev.read(eaddr, 4096) 
-        
         return data
 
 if DEBUG:
@@ -46,3 +39,9 @@ if DEBUG:
 else:
     reader = Reader()
 
+"""
+buf = reader.read(0x12, 6+3+6)
+ds4_mac, unk, host_mac = buf[0:6], buf[6:9], buf[9:15]
+
+print(ds4_mac, host_mac)
+"""
