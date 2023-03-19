f78039cd85f97658a90706588c21f3f211e2f373
Quimzy <bam.boy2020@gmail.com>
2023-03-19 21:06:53 +0300

    we can read packets!

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# dualcontrol: Control Your PC with a DualShock Controller
+# pcshock: Control Your PC with a DualShock Controller
 
 - *Are you tired of having to move from your bed to pause or change a video?*
 
 - *Do you not own a wireless mouse, or feel like they are limited?*
 
-**If you have a DualShock 4 controller, then dualcontrol is for you!**
+**If you have a DualShock 4 controller, then pcshock is for you!**
 
-With dualcontrol, you can control your computer wirelessly from the comfort of your couch or bed.
+With pcshock, you can control your computer wirelessly from the comfort of your couch or bed.
 
---
+++ parser.py
@@ -0,0 +1,48 @@
+
+from hexdump import hexdump
+import usb.core
+import usb.util
+import os
+
+DEBUG = os.getenv('DEBUG')
+
+class Reader:
+    def __init__(self, idv=0x054c, idp=0x09cc):
+        self.dev = usb.core.find(idVendor=idv, idProduct=idp)
+        if not self.dev:
+            raise ConnectionError("Can't connect to device")
+        print(f"Connected to device (VendorID: {idv}, ProductID: {idp})")
+
+        self.dev.reset()
+        if idv == 0x054c and idp == 0x09cc: #for controller
+            self.intf = 3
+            for i in range(self.intf+1):
+                self.detach(i)
+        else:
+            self.intf = 0
+            self.detach()
+
+        self.dev.set_configuration()
+        self.ep = self.get_ep()
+    
+    def detach(self, id=0):
+        if self.dev.is_kernel_driver_active(id):
+            self.dev.detach_kernel_driver(id)
+            print(f"Interface number {id} detached from kernel")
+    
+    def get_ep(self): 
+        ep = self.dev[0][(self.intf,0)][0]
+
+        return ep
+    
+    def read(self):
+        eaddr = self.ep.bEndpointAddress
+        data = self.dev.read(eaddr, 4096) 
+        
+        return data
+
+if DEBUG:
+    reader = Reader(idv=0x275d, idp=0x0ba6) #mouse test
+else:
+    reader = Reader()
+
