cfed6a5f2331ed9d1cec5c07bb2eddc144089c10
Quimzy <bam.boy2020@gmail.com>
2023-03-20 21:01:03 +0300

    we can read actions!

--- parser.py
+++ parser.py
@@ -6,8 +6,6 @@ import os
 
 DEBUG = os.getenv('DEBUG')
 
-DEV_TO_HOST = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_CLASS, usb.util.CTRL_RECIPIENT_INTERFACE)
-
 class Reader:
     def __init__(self, idv=0x054c, idp=0x09cc):
         self.dev = usb.core.find(idVendor=idv, idProduct=idp)
@@ -24,24 +22,29 @@ class Reader:
             self.intf = 0
             self.detach()
 
+        self.dev.set_configuration()
+        self.ep = self.get_ep()
+    
     def detach(self, id=0):
         if self.dev.is_kernel_driver_active(id):
             self.dev.detach_kernel_driver(id)
             print(f"Interface number {id} detached from kernel")
     
-    def read(self, report_id, size):
-        data = self.dev.ctrl_transfer(DEV_TO_HOST, 0x01, report_id, 0, size+1)[1:].tobytes()
+    def get_ep(self): 
+        ep = self.dev[0][(self.intf,0)][0]
 
+        return ep
+    
+    def read(self, size=0x40):
+        #eaddr = self.ep.bEndpointAddress
+        data = self.ep.read(size) 
+        
         return data
 
 if DEBUG:
     reader = Reader(idv=0x275d, idp=0x0ba6) #mouse test
+    print(reader.read(100))
 else:
     reader = Reader()
+    print(reader.read()[8]) #L2
 
-"""
-buf = reader.read(0x12, 6+3+6)
-ds4_mac, unk, host_mac = buf[0:6], buf[6:9], buf[9:15]
-
-print(ds4_mac, host_mac)
-"""
