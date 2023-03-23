e57dfddefe7bf488a3b3d1fb2afeaf44f769fbbd
Quimzy <bam.boy2020@gmail.com>
2023-03-23 04:15:23 +0300

    bluetooth is working

--- errors.py
+++ errors.py
@@ -1,5 +1,15 @@
 
 class ConnectionError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+	def __init__(self, message):
+		self.message = message
+		super().__init__(self.message)
+
+class DiscoveryError(Exception):
+	def __init__(self, message):
+		self.message = message
+		super().__init__(self.message)
+
+class DS4BTError(Exception):
+	def __init__(self, message):
+		self.message = message
+		super().__init__(self.message) 
--- parser.py
+++ parser.py
@@ -1,12 +1,16 @@
 
-from hexdump import hexdump
+
 import usb.core
 import usb.util
+import bluetooth
 import os
+import time
+import socket
+from errors import *
 
 DEBUG = os.getenv('DEBUG')
 
-class Reader:
+class USB:
     def __init__(self, idv=0x054c, idp=0x09cc):
         self.dev = usb.core.find(idVendor=idv, idProduct=idp)
         if not self.dev:
@@ -37,14 +41,59 @@ class Reader:
     
     def read(self, size=0x40):
         #eaddr = self.ep.bEndpointAddress
-        data = self.ep.read(size) 
+        data = self.ep.read(size, timeout=50) 
         
         return data
 
-if DEBUG:
-    reader = Reader(idv=0x275d, idp=0x0ba6) #mouse test
-    print(reader.read(100))
-else:
-    reader = Reader()
-    print(reader.read()[8]) #L2
+class BT:
+    def __init__(self, name="Wireless Controller"):
+        self.name = name
+        self.ds4_addr = self.find_ds4()
+        self.sock = self.connect_ds4()
 
+
+    def find_ds4(self):
+        print("Discovering devices")
+        devices = bluetooth.discover_devices(duration=8, lookup_names=True, flush_cache=True)
+        if len(devices) == 0:
+            raise DiscoveryError("No bluetooth device has been detected")
+
+        ds4_addr = None
+        for addr, name in devices:
+            if name == self.name:
+                ds4_addr = addr
+                print(f"Controller found (address: {ds4_addr})")
+                break
+        
+        if not ds4_addr:
+            raise DS4BTError("DualShock4 device has not been detected")
+
+        return ds4_addr
+
+    def connect_ds4(self):
+        sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET,socket.BTPROTO_L2CAP)
+        print(self.ds4_addr)
+        sock.connect((self.ds4_addr, 0x13))
+
+        return sock
+    
+    def read(self, size=0x40):
+        data = self.sock.recv(size)
+
+        return data
+
+if __name__ == '__main__':
+    if DEBUG:
+        reader = Reader(idv=0x275d, idp=0x0ba6) #mouse test
+        print(reader.read(100))
+    else:
+        if os.getenv('USB'):
+            reader = USB()
+            buf = reader.read()
+        else:
+            reader = BT()
+            buf = reader.read()[2:]
+
+        print(buf[5])
+        print(buf[2])
+        
